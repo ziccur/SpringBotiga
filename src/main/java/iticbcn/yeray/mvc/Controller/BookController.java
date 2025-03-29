332c49982365418ea5b4b6226330fc659c528f29
Yeray <2023_yeray.reyes@iticbcn.cat>
2025-03-29 16:53:00 +0100

    add BookController

---
+++ src/main/java/iticbcn/yeray/mvc/Controller/BookController.java
@@ -0,0 +1,23 @@
+package iticbcn.yeray.mvc.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import iticbcn.yeray.mvc.Repository.RepoLlibre;
+
+@Controller
+@SessionAttributes("users")
+public class BookController {
+
+    @Autowired
+    RepoLlibre repoll = new RepoLlibre();
+
+    @GetMapping("/")
+    public String iniciar(Model model) {
+        return "login";
+    }
+
+}
