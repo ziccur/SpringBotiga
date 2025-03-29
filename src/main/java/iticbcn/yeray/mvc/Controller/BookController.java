32cdfef224d620a3b1d1ca17150a2de8b15df146
Yeray <2023_yeray.reyes@iticbcn.cat>
2025-03-29 17:03:53 +0100

    modify BookController add logout method

--- src/main/java/iticbcn/yeray/mvc/Controller/BookController.java
+++ src/main/java/iticbcn/yeray/mvc/Controller/BookController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import iticbcn.yeray.mvc.Model.Llibre;
 import iticbcn.yeray.mvc.Model.Usuaris;
@@ -134,5 +135,17 @@ public class BookController {
 
         return "cercaid";
 
+    }
+
+    @PostMapping("/logout")
+    public String logout(SessionStatus status) {
+        status.setComplete();
+        return "redirect:/";
+    }
+
+
+    @ModelAttribute("users")
+    public Usuaris getDefaultUser() {
+        return new Usuaris(); 
     }
 }
