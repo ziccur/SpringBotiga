8cebba5ea1964672f2f95ad4436d8afd56014a70
Yeray <2023_yeray.reyes@iticbcn.cat>
2025-03-29 16:55:10 +0100

    modify  BookController add login method

--- src/main/java/iticbcn/yeray/mvc/Controller/BookController.java
+++ src/main/java/iticbcn/yeray/mvc/Controller/BookController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import iticbcn.yeray.mvc.Model.Usuaris;
 import iticbcn.yeray.mvc.Repository.RepoLlibre;
 
 @Controller
@@ -18,6 +21,19 @@ public class BookController {
     @GetMapping("/")
     public String iniciar(Model model) {
         return "login";
+    }
+
+    @PostMapping("/index")
+    public String login(@ModelAttribute("users") Usuaris users, Model model) {
+
+        model.addAttribute("users", users);
+
+        if (users.getUsuari().equals("toni") 
+        && users.getPassword().equals("h3ll0!!")) {
+            return "index";
+        } else {
+            return "login";
+        }        
     }
 
 }
